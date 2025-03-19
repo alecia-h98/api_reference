1c6e32ab1c1078891a652ffbb7d4f0ed3d807e9b
Alecia Hagen <alecia.h98@gmail.com>
2025-03-19 00:39:53 -0500

    possibly fifigured out how to add the soundcloud user playlist to a webpage

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import useStore from '../../zustand/store';
+import './LoginPage.css';
 
 
 function LoginPage() {
@@ -28,6 +29,30 @@ function LoginPage() {
   return (
     <>
       <h2>Login Page</h2>
+      
+      {/*This is the soundcloud link */}
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/573419466&amp;{ ADD YOUR PARAMETERS HERE }">
+      </iframe>
+
+      {/*This is the soundcloud link showing all of my favorites. I have updated the size and width of it. */}
+      <iframe width="50%" height="487" scrolling="no" frameborder="no" allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/60152533/favorites&amp;{ ADD YOUR PARAMETERS HERE }">
+      </iframe>
+
+      {/*This is the soundcloud link that shows all of the songs a person/user has ever uploaded to their spotify */}
+            {/*This was found using their artist name. (Found after the backslash of their soundcloud) This would be really easy to map through someone's information and insert into the code. See example below. */}
+        <iframe width="50%" height="487" scrolling="no" frameborder="no" allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/zephyron21&amp;{ ADD YOUR PARAMETERS HERE }">
+        </iframe>
+
+      {/* Example code */}
+      {/*We will include this code in the map through on the user's information and in their update profile information put we will have them include the username located on their soundcloud profile https://soundcloud.com/zephyron21 using that we will create a insert into the code as shown below*/}
+        <iframe width="50%" height="487" scrolling="no" frameborder="no" allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/${user.soundcloud_id}&amp;{ ADD YOUR PARAMETERS HERE }">
+        </iframe>
+
+      
       <form onSubmit={handleLogIn}>
         <label htmlFor="username">Username:</label>
         <input
